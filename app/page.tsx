626fb7b6d0f011ad854c49a9fe6236949d6114a5
jumnert <chountheachumnith@gmail.com>
2026-02-02 18:20:15 +0700

    Implement Sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import React from "react";
+import AppSidebarClient from "./_AppSideBarClient";
 
 const HomePage = () => {
-  return <div> HomePage</div>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">Jobodia</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton></SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">hdashdhasdh</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 };
 
 export default HomePage;
