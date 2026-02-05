40086fe0e88936815ffb5ef9b7ad8e7bb40fe44f
jumnert <chountheachumnith@gmail.com>
2026-02-05 16:31:59 +0700

    feat: implement Organization Selector

--- app/employer/layout.tsx
+++ app/employer/layout.tsx
@@ -6,14 +6,26 @@ import {
   SidebarGroupLabel,
 } from "@/components/ui/sidebar";
 import { SidebarOrganizationButton } from "@/features/organizations/components/sidebarOrganizationButton";
+import { getCurrentOrganization } from "@/services/clerk/lib/getCurrentAuth";
 import { ClipboardListIcon, PlusIcon } from "lucide-react";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
 export default function EmployeerLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  return (
+    <Suspense>
+      <LayoutSuspense>{children}</LayoutSuspense>
+    </Suspense>
+  );
+}
+async function LayoutSuspense({ children }: { children: React.ReactNode }) {
+  const { orgId } = await getCurrentOrganization();
+  if (orgId == null) return redirect("/organizations/select");
   return (
     <AppSideBar
       content={
