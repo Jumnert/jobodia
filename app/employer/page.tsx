440d0041cc80a69d37935a9a914b7675da606db7
jumnert <chountheachumnith@gmail.com>
2026-02-05 19:00:19 +0700

    feat: employer dashboard

--- app/employer/page.tsx
+++ app/employer/page.tsx
@@ -1,3 +1,37 @@
+import { db } from "@/drizzle/db";
+import { JobListingTable } from "@/drizzle/schema";
+import { getCurrentOrganization } from "@/services/clerk/lib/getCurrentAuth";
+import { desc, eq } from "drizzle-orm";
+import { cacheTag } from "next/cache";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
+
 export default function EmployerHomePage() {
-  return <div>h2</div>;
+  return (
+    <Suspense>
+      <SuspendedPage />
+    </Suspense>
+  );
+}
+
+async function SuspendedPage() {
+  const { orgId } = await getCurrentOrganization();
+  if (orgId === null) return null;
+
+  const jobListing = await getMostRecentJobListing(orgId as string);
+  if (jobListing == null) {
+    redirect("/employer/job-listings/new");
+  } else {
+    redirect(`/employer/job-listings/${jobListing.id}`);
+  }
+}
+
+async function getMostRecentJobListing(orgId: string) {
+  // "use cache"
+  // cacheTag(getJobListingsOrganizationTag(orgId));
+  return db.query.JobListingTable.findFirst({
+    where: eq(JobListingTable.organizationId, orgId),
+    orderBy: desc(JobListingTable.createdAt),
+    columns: { id: true },
+  });
 }
