3c2c200fbbc023f1504bc2418dd5f21f8b1f1b0b
jumnert <chountheachumnith@gmail.com>
2026-02-06 19:27:26 +0700

    fix: add Joblisting Optional

--- features/jobListings/components/jobListingForms.tsx
+++ features/jobListings/components/jobListingForms.tsx
@@ -45,7 +45,7 @@ const NONE_SELECT_VALUE = "none";
 export function JobListingForm({
   jobListing,
 }: {
-  jobListing: Pick<
+  jobListing?: Pick<
     typeof JobListingTable.$inferSelect,
     | "title"
     | "description"
