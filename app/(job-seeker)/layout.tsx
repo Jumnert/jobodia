0d8d87180928d2e434db23cee4db323bc59143f9
jumnert <chountheachumnith@gmail.com>
2026-02-10 10:20:17 +0700

    feat: main job board sidebar

--- app/(job-seeker)/layout.tsx
+++ app/(job-seeker)/layout.tsx
@@ -7,38 +7,44 @@ import {
   LayoutDashboard,
   LogInIcon,
 } from "lucide-react";
+import { ReactNode } from "react";
 
 export default function JobSeekerLayout({
   children,
+  sidebar,
 }: {
   children: React.ReactNode;
+  sidebar: ReactNode;
 }) {
   return (
     <AppSideBar
       content={
-        <SideBarNavMenuGroup
-          className="mt-auto"
-          items={[
-            { href: "/", icon: <ClipboardListIcon />, label: "Job Board" },
-            {
-              href: "/ai-search",
-              icon: <BrainCircuitIcon />,
-              label: "Ai Search",
-            },
-            {
-              href: "/employer",
-              icon: <LayoutDashboard />,
-              label: "Employer Dashboard",
-              authStatus: "signedIn",
-            },
-            {
-              href: "/sign-in",
-              icon: <LogInIcon />,
-              label: "Sign In",
-              authStatus: "signedOut",
-            },
-          ]}
-        />
+        <>
+          {sidebar}
+          <SideBarNavMenuGroup
+            className="mt-auto"
+            items={[
+              { href: "/", icon: <ClipboardListIcon />, label: "Job Board" },
+              {
+                href: "/ai-search",
+                icon: <BrainCircuitIcon />,
+                label: "Ai Search",
+              },
+              {
+                href: "/employer",
+                icon: <LayoutDashboard />,
+                label: "Employer Dashboard",
+                authStatus: "signedIn",
+              },
+              {
+                href: "/sign-in",
+                icon: <LogInIcon />,
+                label: "Sign In",
+                authStatus: "signedOut",
+              },
+            ]}
+          />
+        </>
       }
       footerButton={<SidebarUserButton />}
     >
